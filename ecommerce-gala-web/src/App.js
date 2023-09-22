cd8f22538665c736524040c980c5d54e0f1ef5af
sovortey <126035238+sovortey514@users.noreply.github.com>
2023-09-22 12:39:23 +0700

    third commit

--- ecommerce-gala-web/src/App.js
+++ ecommerce-gala-web/src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import HomePage from './page/home/HomePage';
+import ProductPage from './page/product/ProductPage ';
 
 function App() {
   return (
     <div>
-     hello
+     <ProductPage/>
+     <HomePage/>
     </div>
   );
 }
---
+++ ecommerce-gala-web/src/page/product/ProductPage .jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductPage  = () => {
+  return (
+    <div>
+      hello product
+    </div>
+  )
+}
+
+export default ProductPage 
