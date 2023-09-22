4a475ba83eed04a5e3fd5ddb9ee5c269b5a2502a
sovortey <126035238+sovortey514@users.noreply.github.com>
2023-09-22 12:11:49 +0700

    first commit

---
+++ ecommerce-gala-web/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import HomePage from './page/home/HomePage';
+
+function App() {
+  return (
+    <div>
+     
+      <HomePage/>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ ecommerce-gala-web/src/page/home/HomePage.js
@@ -0,0 +1,9 @@
+import "./HomePage.css"
+const HomePage = () =>{
+    return(
+        <div className = "home_page">
+            <h1>HomePAge</h1>
+        </div>
+    );
+}
+export default HomePage;
